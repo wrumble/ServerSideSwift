91acd4d3abdba8925528da6b71bc330c38a0d3f8
Wayne <rumblewayne@gmail.com>
2019-09-13 21:19:40 +0100

    Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM swiftdocker/swift
 ADD . /ServerSideSwift
 WORKDIR /ServerSideSwift
-RUN swift build --configuration release
+CMD swift build --configuration release
 EXPOSE 8080
 ENTRYPOINT [".build/release/ServerSideSwift"]
--- Sources/ServerSideSwift/main.swift
+++ Sources/ServerSideSwift/main.swift
@@ -1,11 +1,28 @@
 import Kitura
 
+struct Movie: Codable {
+    let title: String
+}
+
 let router = Router()
 
 router.get("/") { request, response, next in
     response.send("Hello Waynes world!")
     next()
 }
+
+router.post("/hello") { request, response, next in
+    print("Request body: \(request.body)")
+    do {
+        let movie = try request.read(as: Movie.self)
+        response.send("Hello \(movie.title)!")
+    } catch let error {
+        response.send("Couldnt read movie: \(error)")
+    }
+    
+    next()
+}
+
 
 Kitura.addHTTPServer(onPort: 8080, with: router)
 Kitura.run()
